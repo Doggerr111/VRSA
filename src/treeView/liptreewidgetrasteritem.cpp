018b5cbc773cc449f6b5d11b8ba575cfb5a5bce2
doger <doger@doger.myguest.virtualbox.org>
2024-03-27 20:25:56 +0300

    new tree items for raster layers

---
+++ src/treeView/liptreewidgetrasteritem.cpp
@@ -0,0 +1,7 @@
+#include "liptreewidgetrasteritem.h"
+
+LIPTreeWidgetRasterItem::LIPTreeWidgetRasterItem()
+{
+    setIcon(0,QIcon(":/images/icons/rasterTreeItem.png"));
+
+}
---
+++ src/treeView/liptreewidgetrasteritem.h
@@ -0,0 +1,13 @@
+#ifndef LIPTREEWIDGETRASTERITEM_H
+#define LIPTREEWIDGETRASTERITEM_H
+#include "liptreewidgetitem.h"
+
+class LIPTreeWidgetRasterItem: public LIPTreeWidgetItem
+{
+public:
+    LIPTreeWidgetRasterItem();
+private:
+    QIcon mRasterIcon;
+};
+
+#endif // LIPTREEWIDGETRASTERITEM_H
