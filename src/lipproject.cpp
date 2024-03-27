bf7d70cc6e548c20739432b2ffb555a3f5608210
doger <doger@doger.myguest.virtualbox.org>
2024-03-27 20:31:50 +0300

    add project settings

--- src/lipproject.cpp
+++ src/lipproject.cpp
@@ -63,9 +63,9 @@ void LIPProject::deleteVectorByPath(QString path)
     {
         if (vectorLayers.at(i)->returnFileName()==path)
         {
-
-            delete vectorLayers.at(i);
+            auto lToDel=vectorLayers.at(i);
             vectorLayers.removeAt(i);
+            delete lToDel;
             //
             //vectorLayers.erase(1);
         }
@@ -107,7 +107,7 @@ LIPVectorLayer *LIPProject::getVectorLayerByPath(QString path)
         {
             return vectorLayers.at(i);
         }
-        qDebug()<< "VECT:" + QString::number(i);
+
     }
     return nullptr;
 }
@@ -122,6 +122,81 @@ LIPVectorLayer* LIPProject::getActiveLayer()
     }
 }
 
+void LIPProject::addRasterLayer(LIPRasterLayer *rasterLayer)
+{
+    rasterLayers.append(rasterLayer);
+}
+
+void LIPProject::deleteRasterLayerByPath(QString path)
+{
+    for (int i=0;i<rasterLayers.size();i++)
+    {
+        if (rasterLayers.at(i)->getFileName()==path)
+        {
+
+            delete rasterLayers.at(i);
+            rasterLayers.removeAt(i);
+
+        }
+    }
+}
+
+QVector<LIPRasterLayer *> LIPProject::getRasterLayers()
+{
+    return rasterLayers;
+}
+
+LIPRasterLayer *LIPProject::getRasterLayerByPath(QString path)
+{
+    for (int i=0; i<rasterLayers.size(); i++)
+    {
+        if (rasterLayers.at(i)->getFileName()==path)
+        {
+            return rasterLayers.at(i);
+        }
+
+    }
+    return nullptr;
+}
+
+void LIPProject::setActivePostGISConnection(GDALDataset *ds)
+{
+    if (ds!=nullptr)
+        mActivePostGISConnection=ds;
+
+}
+
+void LIPProject::disconnectPostGISConnection()
+{
+    GDALClose(mActivePostGISConnection);
+    mActivePostGISConnection=nullptr;
+}
+
+GDALDataset *LIPProject::getPostGISDataSet()
+{
+    return mActivePostGISConnection;
+}
+
+bool LIPProject::isDatasetInUse(GDALDataset *dS)
+{
+
+    for (int i=0; dS->GetLayerCount(); i++)
+    {
+        for (int j=0; j<vectorLayers.size(); j++)
+        {
+            if (dS->GetLayer(i)==vectorLayers.at(j)->getOGRLayer())
+            {
+                qDebug()<< dS->GetLayer(i);
+                qDebug()<<vectorLayers.at(j)->getOGRLayer();
+                if (dS->GetLayer(i)==nullptr)
+                    return false;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 bool LIPProject::addCoordinateSystem(LIPCoordinateSystem *crs)
 {
     //TODO добавить проверку на корректность системы
@@ -133,6 +208,177 @@ QVector<LIPCoordinateSystem*> LIPProject::getCoordinateSystems()
 {
     return CRSystems;
 }
+
+bool LIPProject::setProjectCRS(LIPCoordinateSystem *s)
+{
+    if (s==nullptr)
+        return false;
+    mProjectCRS=s;
+    emit crsChanged();
+    return true;
+}
+
+LIPCoordinateSystem *LIPProject::getProjectCRS()
+{
+    return mProjectCRS;
+}
+
+bool LIPProject::saveProject(QString fileName)
+{
+    QFile file(fileName);
+    if (!file.open(QIODevice::WriteOnly))
+    {
+        LIPWidgetManager::getInstance().showMessage("Невозможно сохранить проект", 1000, Error);
+        return false;
+    }
+
+    QDataStream out(&file);
+    out.setVersion(QDataStream::Qt_5_15); // Устанавливаем версию для совместимости
+    // Записываем количество векторных слоёв.
+    out << static_cast<quint32>(vectorLayers.size());
+    // Записываем каждый путь в файл
+    for (int i=0; i<vectorLayers.size(); i++)
+    {
+        out << vectorLayers.at(i)->returnFileName();
+    }
+    // Записываем количество растровых слоёв.
+    out << static_cast<quint32>(rasterLayers.size());
+    // Записываем каждый путь в файл
+    for (int i=0; i<rasterLayers.size(); i++)
+    {
+        out << rasterLayers.at(i)->getFileName();
+    }
+
+    file.close();
+
+}
+
+bool LIPProject::openProject(QString fileName)
+{
+    QFile file(fileName);
+    if (!file.open(QIODevice::ReadOnly))
+    {
+        LIPWidgetManager::getInstance().showMessage("Невозможно прочитать файл проекта", 1000, Error);
+        return false;
+    }
+    QDataStream in(&file);
+    in.setVersion(QDataStream::Qt_5_15); // Устанавливаем версию для совместимости
+
+
+    quint32 vectorNumPaths;
+    in >> vectorNumPaths;
+    QStringList vectorList;
+
+    for (quint32 i = 0; i < vectorNumPaths; ++i) {
+        QString path;
+        in >> path;
+        //qDebug()<<path;
+        vectorList.append(path);
+    }
+    LIPVectorReader::readLayersFromStringList(vectorList);
+    quint32 rasterNumPaths;
+    in >> rasterNumPaths;
+
+    for (quint32 i = 0; i < rasterNumPaths; ++i) {
+        QString path;
+        in >> path;
+        //qDebug()<<path;
+        new LIPRasterLayer(path);
+    }
+
+    file.close();
+
+
+    return true;
+}
+
+void LIPProject::addAction(QAction *action)
+{
+    mGUISettings.mainToolBarActions.append(action);
+    LIPWidgetManager::getInstance().getMainWindow();
+}
+
+void LIPProject::deleteAction(QAction *delAction)
+{
+    for(int actIt=0; actIt<mGUISettings.mainToolBarActions.size(); actIt++)
+    {
+        if (mGUISettings.mainToolBarActions.at(actIt)==delAction)
+        {
+            QVariant v = delAction->property("associatedButton");
+            QPushButton* associatedButton = qobject_cast<QPushButton*>(v.value<QWidget*>());
+            if (associatedButton)
+                delete associatedButton;
+
+            //delete mGUISettings.mainToolBarActions.at(actIt);
+            mGUISettings.mainToolBarActions.removeAt(actIt);
+
+        }
+    }
+}
+
+bool LIPProject::isActionAdded(QAction *action)
+{
+    for(int actIt=0; actIt<mGUISettings.mainToolBarActions.size(); actIt++)
+    {
+        if (mGUISettings.mainToolBarActions.at(actIt)==action)
+            return true;
+    }
+    return false;
+}
+
+void LIPProject::setActions(QVector<QAction *>)
+{
+
+}
+
+void LIPProject::setVectorDataFolder(QString path)
+{
+    mVectorFolder = path;
+}
+
+void LIPProject::setRasterDataFolder(QString path)
+{
+    mRasterFolder = path;
+}
+
+QString LIPProject::getVectorDataFolder()
+{
+    return mVectorFolder;
+}
+
+QString LIPProject::getRasterDataFolder()
+{
+    return mRasterFolder;
+}
+
+void LIPProject::setMapCanvasColor(QColor col)
+{
+    if (col.isValid())
+        mMapCanvasColor = col;
+    else
+        mMapCanvasColor = Qt::white;
+    LIPWidgetManager::getInstance().getScene()->setBackgroundBrush(QBrush(mMapCanvasColor));
+}
+
+void LIPProject::setAntiAliasing(bool flag)
+{
+    mAntiAliasingFlag = flag;
+    LIPWidgetManager::getInstance().getView()->setRenderHint(QPainter::Antialiasing, mAntiAliasingFlag);
+
+
+}
+
+QColor LIPProject::getMapCanvasColor()
+{
+    return mMapCanvasColor;
+}
+
+bool LIPProject::isAntiAliasingOn()
+{
+    return mAntiAliasingFlag;
+}
+
+
 
 
 
--- src/lipproject.h
+++ src/lipproject.h
@@ -1,15 +1,23 @@
 #ifndef LIPPROJECT_H
 #define LIPPROJECT_H
 #include <QObject>
-#include <vector/lipvectorlayer.h>
-#include <lipcoordinatesystem.h>
+#include "vector/lipvectorlayer.h"
+#include "lipcoordinatesystem.h"
+#include "liprasterlayer.h"
+#include <QFile>
+#include <QAction>
 class LIPProject : public QObject
 {
     Q_OBJECT
 public:
     static LIPProject &getInstance();
 
-    //layers
+    struct guiSettings
+    {
+        QVector<QAction*> mainToolBarActions;
+    };
+
+    //vector layers
     void addVectorLayer(LIPVectorLayer* vect);
     void addVectorLayers(QVector<LIPVectorLayer*> vects);
     void setVectorLayers(QVector<LIPVectorLayer*> vects);
@@ -24,22 +32,77 @@ public:
     LIPVectorLayer* getVectorLayerByPath(QString path);
     LIPVectorLayer* getActiveLayer();
 
+    //rasterLayers
+    void addRasterLayer(LIPRasterLayer* rasterLayer);
+    void addRasterLayers(QVector<LIPVectorLayer*> rasterLayers);
+    void setRasterLayers(QVector<LIPVectorLayer*> rasterLayers);
+    void deleteRasterLayerByPath(QString path);
+
+    QVector<LIPRasterLayer*> getRasterLayers();
+    LIPRasterLayer* getRasterLayerByIndx(int index);
+    LIPRasterLayer* getRasterLayerByName(QString name);
+    LIPRasterLayer *getRasterLayerByPath(QString path);
+
+    //database
+    void setActivePostGISConnection(GDALDataset* ds);
+    void disconnectPostGISConnection();
+    /** Получение набора слоев из базы данных. Функция возвращает nullptr, если нет активного соединения*/
+    GDALDataset *getPostGISDataSet();
+
+
+    bool isDatasetInUse(GDALDataset* dS);
+
     //SRC
     bool addCoordinateSystem(LIPCoordinateSystem *crs);
-
     QVector<LIPCoordinateSystem*> getCoordinateSystems();
+    bool setProjectCRS(LIPCoordinateSystem* s);
+    LIPCoordinateSystem *getProjectCRS();
+
+    //project save
+    bool saveProject(QString filename);
+    bool openProject(QString filename);
+
+
+    //gui
+    void addAction(QAction* action);
+    void deleteAction(QAction* action);
+    bool isActionAdded(QAction* action);
+    void setActions(QVector<QAction*>);
+
+    //path settings
+    void setVectorDataFolder(QString);
+    void setRasterDataFolder(QString);
+
+    QString getVectorDataFolder();
+    QString getRasterDataFolder();
+
+
+    //map settings
+    void setMapCanvasColor(QColor col);
+    void setAntiAliasing(bool flag);
+
+    QColor getMapCanvasColor();
+    bool isAntiAliasingOn();
 
 public slots:
     void redrawNeeded(double);
 
+
 signals:
     void newCRS();
+    void crsChanged();
 private:
     LIPVectorLayer* activeLayer; //
     QVector<LIPVectorLayer*> vectorLayers;
-
+    QVector<LIPRasterLayer*> rasterLayers;
     QVector<LIPCoordinateSystem*> CRSystems;
-
+    GDALDataset* mActivePostGISConnection;
+    LIPCoordinateSystem* mProjectCRS;
+    guiSettings mGUISettings;
+    QString mVectorFolder;
+    QString mRasterFolder;
+    QColor mMapCanvasColor = Qt::white;
+    bool mAntiAliasingFlag = false;
 private:
     LIPProject() = default;
     ~LIPProject() = default;
