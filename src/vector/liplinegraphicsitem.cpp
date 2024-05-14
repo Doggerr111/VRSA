ef75d6c29c11a883c52a6848bd97482ac03a4f15
doger <doger@doger.myguest.virtualbox.org>
2024-05-14 23:14:28 +0300

    fix boundingRect() method for correct visualisation

--- src/vector/liplinegraphicsitem.cpp
+++ src/vector/liplinegraphicsitem.cpp
@@ -46,8 +46,27 @@ void LIPLineGraphicsItem::calculateBoundingRect()
 
 QRectF LIPLineGraphicsItem::boundingRect() const
 {
-    //return QRectF();
-    return(QPolygonF(vect).boundingRect());
+    //return(QPolygonF(vect).boundingRect());
+
+    QPolygonF polyLine = vect; // Ваши координаты точек линии
+        // Находим минимальное и максимальное значение x и y
+        qreal minX = polyLine.first().x();
+        qreal maxX = polyLine.first().x();
+        qreal minY = polyLine.first().y();
+        qreal maxY = polyLine.first().y();
+
+        for (const QPointF& point : polyLine) {
+            minX = qMin(minX, point.x());
+            maxX = qMax(maxX, point.x());
+            minY = qMin(minY, point.y());
+            maxY = qMax(maxY, point.y());
+        }
+
+        qreal penWidth =LIPVectorStyle::MMToPixel(mStyle->getPen().widthF()/mSceneScale) ; // Ширина линии, которую используете
+        qreal halfPenWidth = penWidth / 2.0; // Половина толщины линии
+
+        // Расширяем boundingRect() на половину толщины линии во всех направлениях
+        return QRectF(minX - halfPenWidth, minY - halfPenWidth, maxX - minX + penWidth, maxY - minY + penWidth);
 }
 
 void LIPLineGraphicsItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
@@ -63,6 +82,7 @@ void LIPLineGraphicsItem::paint(QPainter *painter, const QStyleOptionGraphicsIte
     else
     {
         QPen pen = mStyle->getPen();
+        pen.setCapStyle(Qt::SquareCap);
         pen.setWidthF(LIPVectorStyle::MMToPixel(pen.widthF())/mSceneScale);
         painter->setPen(pen);
     }
